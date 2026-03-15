e8c74a74cb3a5c8a89c192a2d51cd03035d461ea
astra-kawa <mjagodzski@gmail.com>
2026-03-15 13:28:39 -0400

    working on getting constellation

--- backend/crates/adapters/src/postgres/constellation_table.sql
+++ backend/crates/adapters/src/postgres/constellation_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS constellation (
-    id TEXT PRIMARY KEY,
+    name TEXT PRIMARY KEY,
     initial TIMESTAMP,
     pos_x REAL,
     pos_y REAL,
--- backend/crates/adapters/src/postgres/postgres_repository.rs
+++ backend/crates/adapters/src/postgres/postgres_repository.rs
@@ -1,4 +1,5 @@
-use domain::models::{Constellation, Satellite};
+use async_trait::async_trait;
+use domain::models::{Constellation, EciState, Satellite};
 use ports::{errors::RepositoryError, outbound::ConstellationRepository};
 use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 
@@ -22,8 +23,12 @@ impl PostgresRepository {
     }
 }
 
+#[async_trait]
 impl ConstellationRepository for PostgresRepository {
-    async fn set_constellation(&self, constellation: Constellation) -> Result<(), RepositoryError> {
+    async fn set_constellation(
+        &self,
+        _constellation: Constellation,
+    ) -> Result<(), RepositoryError> {
         // for satellite in constellation.satellites {
         //     sqlx::query!(
         //         "INSERT INTO constellation VALUES (id, initial, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z), ($1, $2, $3, $4, $5, $6, $7, $8)"
@@ -34,6 +39,27 @@ impl ConstellationRepository for PostgresRepository {
     }
 
     async fn get_constellation(&self) -> Result<Constellation, RepositoryError> {
+        let satellite_records = sqlx::query!("SELECT * FROM constellation")
+            .fetch_all(&self.pool)
+            .await
+            .map_err(|_| RepositoryError::Other)?;
+
+        let mut satellites = Vec::<Satellite>::new();
+        for record in satellite_records {
+            satellites.push(Satellite {
+                id: record.name,
+                initial_state: EciState {
+                    epoch: record.initial.unwrap(),
+                    pos_x: record.pos_x.unwrap(),
+                    pos_y: record.pos_y.unwrap(),
+                    pos_z: record.pos_z.unwrap(),
+                    vel_x: record.vel_x.unwrap(),
+                    vel_y: record.vel_y.unwrap(),
+                    vel_z: record.vel_z.unwrap(),
+                },
+            });
+        }
+
         Ok(Constellation {
             satellites: Vec::<Satellite>::new(),
         })
