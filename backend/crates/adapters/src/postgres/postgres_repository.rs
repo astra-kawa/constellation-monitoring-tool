164ecd0e97d5f51e470427d7ce2809d5e72119d5
astra-kawa <mjagodzski@gmail.com>
2026-05-23 16:03:55 -0400

    implemented jsonb data type for constellation table

--- backend/crates/adapters/src/postgres/postgres_repository.rs
+++ backend/crates/adapters/src/postgres/postgres_repository.rs
@@ -1,28 +1,14 @@
 use async_trait::async_trait;
-use chrono::{DateTime, Utc};
-use domain::models::{Constellation, EciState, Satellite};
+use domain::models::{Constellation, Satellite};
 use ports::{errors::RepositoryError, outbound::ConstellationRepository};
 use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 
-const SET_SATELLITE_QUERY: &str =
-    "INSERT INTO constellation (name, initial, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z)
-VALUES ($1, $2::timestamp, $3, $4, $5, $6, $7, $8)
-ON CONFLICT (name) DO UPDATE SET
-    initial = EXCLUDED.initial,
-    pos_x = EXCLUDED.pos_x,
-    pos_y = EXCLUDED.pos_y,
-    pos_z = EXCLUDED.pos_z,
-    vel_x = EXCLUDED.vel_x,
-    vel_y = EXCLUDED.vel_y,
-    vel_z = EXCLUDED.vel_z
+const SET_SATELLITE_QUERY: &str = "INSERT INTO constellation_test (id, data)
+VALUES ($1, $2)
+ON CONFLICT (id) DO UPDATE SET
+    data = EXCLUDED.data,
 WHERE
-    constellation.initial IS DISTINCT FROM EXCLUDED.initial OR
-    constellation.pos_x IS DISTINCT FROM EXCLUDED.pos_x OR
-    constellation.pos_y IS DISTINCT FROM EXCLUDED.pos_y OR
-    constellation.pos_z IS DISTINCT FROM EXCLUDED.pos_z OR
-    constellation.vel_x IS DISTINCT FROM EXCLUDED.vel_x OR
-    constellation.vel_y IS DISTINCT FROM EXCLUDED.vel_y OR
-    constellation.vel_z IS DISTINCT FROM EXCLUDED.vel_z";
+    constellation_test.data IS DISTINCT FROM EXCLUDED.data";
 
 pub struct PostgresRepository {
     pool: Pool<Postgres>,
@@ -44,15 +30,12 @@ impl PostgresRepository {
 impl ConstellationRepository for PostgresRepository {
     async fn set_constellation(&self, constellation: Constellation) -> Result<(), RepositoryError> {
         for satellite in constellation.satellites {
+            let data_string =
+                serde_json::to_string(&satellite.data).map_err(|_| RepositoryError::Other)?;
+
             sqlx::query(SET_SATELLITE_QUERY)
                 .bind(satellite.id)
-                .bind(satellite.initial_state.epoch.naive_utc().to_string())
-                .bind(satellite.initial_state.pos_x)
-                .bind(satellite.initial_state.pos_y)
-                .bind(satellite.initial_state.pos_z)
-                .bind(satellite.initial_state.vel_x)
-                .bind(satellite.initial_state.vel_y)
-                .bind(satellite.initial_state.vel_z)
+                .bind(data_string)
                 .execute(&self.pool)
                 .await
                 .map_err(|_| RepositoryError::Other)?;
@@ -62,7 +45,7 @@ impl ConstellationRepository for PostgresRepository {
     }
 
     async fn get_constellation(&self) -> Result<Constellation, RepositoryError> {
-        let satellite_records = sqlx::query!("SELECT * FROM constellation ORDER BY name ASC")
+        let satellite_records = sqlx::query!("SELECT * FROM constellation_test ORDER BY id ASC")
             .fetch_all(&self.pool)
             .await
             .map_err(|_| RepositoryError::Other)?;
@@ -70,20 +53,9 @@ impl ConstellationRepository for PostgresRepository {
         let mut satellites = Vec::<Satellite>::new();
         for record in satellite_records {
             satellites.push(Satellite {
-                id: record.name,
-                initial_state: EciState {
-                    epoch: {
-                        let epoch = record.initial.unwrap().assume_utc();
-                        DateTime::<Utc>::from_timestamp(epoch.unix_timestamp(), epoch.nanosecond())
-                            .expect("database timestamp should be valid")
-                    },
-                    pos_x: record.pos_x.unwrap(),
-                    pos_y: record.pos_y.unwrap(),
-                    pos_z: record.pos_z.unwrap(),
-                    vel_x: record.vel_x.unwrap(),
-                    vel_y: record.vel_y.unwrap(),
-                    vel_z: record.vel_z.unwrap(),
-                },
+                id: record.id,
+                data: serde_json::from_str(&record.data.to_string())
+                    .expect("JSON was not well-formatted"),
             });
         }
 
@@ -91,15 +63,12 @@ impl ConstellationRepository for PostgresRepository {
     }
 
     async fn set_satellite(&self, satellite: Satellite) -> Result<(), RepositoryError> {
+        let data_string =
+            serde_json::to_string(&satellite.data).map_err(|_| RepositoryError::Other)?;
+
         sqlx::query(SET_SATELLITE_QUERY)
             .bind(satellite.id)
-            .bind(satellite.initial_state.epoch.naive_utc().to_string())
-            .bind(satellite.initial_state.pos_x)
-            .bind(satellite.initial_state.pos_y)
-            .bind(satellite.initial_state.pos_z)
-            .bind(satellite.initial_state.vel_x)
-            .bind(satellite.initial_state.vel_y)
-            .bind(satellite.initial_state.vel_z)
+            .bind(data_string)
             .execute(&self.pool)
             .await
             .map_err(|err| RepositoryError::QueryError(err.to_string()))?;
@@ -108,28 +77,18 @@ impl ConstellationRepository for PostgresRepository {
     }
 
     async fn get_satellite(&self, satellite_id: &str) -> Result<Satellite, RepositoryError> {
-        let satellite_record =
-            sqlx::query!("SELECT * FROM constellation WHERE name = $1", satellite_id)
-                .fetch_one(&self.pool)
-                .await
-                .map_err(|_| RepositoryError::Other)?;
+        let satellite_record = sqlx::query!(
+            "SELECT * FROM constellation_test WHERE id = $1",
+            satellite_id
+        )
+        .fetch_one(&self.pool)
+        .await
+        .map_err(|_| RepositoryError::Other)?;
 
-        // todo - replace with conversion function
         let satellite = Satellite {
-            id: satellite_record.name,
-            initial_state: EciState {
-                epoch: {
-                    let epoch = satellite_record.initial.unwrap().assume_utc();
-                    DateTime::<Utc>::from_timestamp(epoch.unix_timestamp(), epoch.nanosecond())
-                        .expect("database timestamp should be valid")
-                },
-                pos_x: satellite_record.pos_x.unwrap(),
-                pos_y: satellite_record.pos_y.unwrap(),
-                pos_z: satellite_record.pos_z.unwrap(),
-                vel_x: satellite_record.vel_x.unwrap(),
-                vel_y: satellite_record.vel_y.unwrap(),
-                vel_z: satellite_record.vel_z.unwrap(),
-            },
+            id: satellite_record.id,
+            data: serde_json::from_str(&satellite_record.data.to_string())
+                .expect("JSON was not well-formatted"),
         };
 
         Ok(satellite)
